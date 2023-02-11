47aba3a42d8534ed678a8bba9164738f251225ad
Luis Leyva <70904067+Luis-Leyva@users.noreply.github.com>
2023-02-11 01:28:38 -0600

    Changed to env file and changed keys

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAR2y_FQkkQS21hQ-J1QxH8vWNvkOfK7zw",
-    authDomain: "overture-scouting-app.firebaseapp.com",
-    databaseURL: "https://overture-scouting-app-default-rtdb.firebaseio.com",
-    projectId: "overture-scouting-app",
-    storageBucket: "overture-scouting-app.appspot.com",
-    messagingSenderId: "372276289782",
-    appId: "1:372276289782:web:1f85f741ff131ab2fd0cd7",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
