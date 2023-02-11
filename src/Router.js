a6f454e8f7fc33bf4395f523d9c50dee7a2f54ca
Diego Saldaña <diegosaldana@outlook.com>
2023-02-11 13:22:46 -0600

    Updated Router

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NavigationBar from "./Components/navigationBar/NavigationBar";
 import Home from "./pages/Home/Home";
-import Pits from "./page/Pits/Pits";
+import Pits from "./pages/Pits/Pits";
 
 const Router = () => {
     return(
